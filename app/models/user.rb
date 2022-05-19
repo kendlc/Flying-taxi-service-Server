1e53edefb46f8ffbb32d6842015967d7f5cbcc0d
kendlc <kennethcdlc@gmail.com>
2022-05-20 09:38:49 +1000

    fixed email validation regex, detect error when there no @ and .

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
     validates :first_name, :presence => true, length: { :minimum => 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "A name cannot contain a number/ special character" }
     validates :last_name, :presence => true, length: { :minimum => 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "A name cannot contain a number/ special character" }
-    validates :email, :presence => true, :uniqueness => true, format: { with: /\A[a-zA-Z.@]+\z/, message: "Email needs one @ and atleast one '.'" }
+    validates :email, :presence => true, :uniqueness => true, format: { with: /\A\S+@.+\.\S+\z/, message: "A name cannot contain a number/ special character" }
     # validates :password, :length => {:within => 6..40}, confirmation: true, unless: Proc.new { |user| user.password.blank? }
 
     # has_secure_password
