5e80731c24ef6a3e0542a5e0ef67213ff356ac8d
kendlc <kennethcdlc@gmail.com>
2022-05-19 19:06:17 +1000

    added name and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # require 'bcrypt'
 
 class User < ApplicationRecord
-    validates :first_name, :presence => true, length: { :within => 2..40}
-    validates :last_name, :presence => true, length: { :within => 2..40}
-    validates :email, :presence => true, :uniqueness => true
+    validates :first_name, :presence => true, length: { :minimum: 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "first name only allows letters" }
+    validates :last_name, :presence => true, length: { :minimum: 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "last name only allows letters" }
+    validates :email, :presence => true, :uniqueness => true, , format: { with: /\A[a-zA-Z.]+\z/, message: "" }
     # validates :password, :length => {:within => 6..40}, confirmation: true, unless: Proc.new { |user| user.password.blank? }
 
     # has_secure_password
