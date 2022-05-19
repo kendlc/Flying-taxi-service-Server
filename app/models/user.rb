3e5036fc0bf8d28f83071f9c521e61f9d763aba4
kendlc <kennethcdlc@gmail.com>
2022-05-19 23:37:34 +1000

    change error message to A name cannot contain a number/ special character

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # require 'bcrypt'
 
 class User < ApplicationRecord
-    validates :first_name, :presence => true, length: { :minimum => 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "First name only allows letters" }
-    validates :last_name, :presence => true, length: { :minimum => 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "Last name only allows letters" }
+    validates :first_name, :presence => true, length: { :minimum => 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "A name cannot contain a number/ special character" }
+    validates :last_name, :presence => true, length: { :minimum => 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "A name cannot contain a number/ special character" }
     validates :email, :presence => true, :uniqueness => true, format: { with: /\A[a-zA-Z.@]+\z/, message: "Email needs one @ and atleast one '.'" }
     # validates :password, :length => {:within => 6..40}, confirmation: true, unless: Proc.new { |user| user.password.blank? }
 
